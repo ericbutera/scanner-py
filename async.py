e1235412a6626f2632f768650c400b374cc3becf
Eric Butera <eric@ericbutera.com>
2023-03-26 15:13:19 -0400

    fix: asyncio example works now

--- async.py
+++ async.py
@@ -9,16 +9,26 @@ from cloudevents.conversion import to_json
 from shared import (API_URL, ENDPOINT_URL, PAYLOAD_ENDPOINT, TIMEOUT,
                     VERIFY_SSL, bucket_to_event, list_buckets)
 
+headers = {
+    "Content-Type": "application/json",
+    "X-API-KEY": os.getenv("API_KEY"),
+}
+
 
 async def main():
     print("starting async io")
     start = time.perf_counter()
 
-    # this blocks per call for some reason
-    async with _get_session() as session:
+    # TODO this will OOM; need to use a semaphore
+    async with aiohttp.ClientSession(base_url=API_URL, headers=headers) as session:
+        tasks = []
         for bucket in list_buckets(ENDPOINT_URL):
-            event = bucket_to_event(bucket)
-            await emit(session, event)
+            ev = bucket_to_event(bucket)
+            task = emit(session, ev)
+            tasks.append(asyncio.ensure_future(task))
+
+        results = await asyncio.gather(*tasks)
+        print(results)
 
     end = time.perf_counter()
     delta = end - start
@@ -28,29 +38,16 @@ async def main():
 async def emit(session: ClientSession, event):
     """emit"""
     # headers, body = to_binary(event)
-    headers = {
-        "Content-Type": "application/json",
-        "X-API-KEY": os.getenv("API_KEY"),
-    }
     body = to_json(event, data_marshaller=str)
-
     async with session.post(
         PAYLOAD_ENDPOINT,
-        headers=headers,
+        # headers=headers,
         data=body,
         verify_ssl=VERIFY_SSL,
         timeout=TIMEOUT
-    ) as response:
-        # print("status ", response.status)
-        return response
+    ) as resp:
+        return await resp.text()
 
-
-async def _get_session():
-    headers = {
-        "Content-Type": "application/json",
-        "X-API-KEY": os.getenv("API_KEY"),
-    }
-    return aiohttp.ClientSession(base_url=API_URL, headers=headers)
 
 if __name__ == "__main__":
     asyncio.run(main())
